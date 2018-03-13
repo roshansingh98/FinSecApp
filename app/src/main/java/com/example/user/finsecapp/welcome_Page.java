38f0f0d74cdf77fd9510c899bdf009250738e420
Roshan Singh <singhroshan98@gmail.com>
2018-03-13 15:29:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/finsecapp/welcome_Page.java
@@ -0,0 +1,29 @@
+package com.example.user.finsecapp;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class welcome_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome__page);
+
+        new CountDownTimer(2000,1000){
+            public void onTick(long milliuntildone)
+            {
+               // Log.i("time passed",String.valueOf(milliuntildone/1000));
+            }
+            public void onFinish(){
+                Intent intent=new Intent(getApplicationContext(),mainPage.class);
+                startActivity(intent);
+            }
+
+        }.start();
+
+    }
+}
